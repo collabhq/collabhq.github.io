92ecb017c53833a7eed305233c0a595aafafa0b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-10 22:02:36 +0000

    Deploy puffnote/puffnote.github.io to github.com/puffnote/puffnote.github.io.git:master

---
+++ precache-manifest.55cd7e48e0f8d34cd19ccefa25e89125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50955253534b6d2eb55",
+    "url": "/static/css/main.4d6cb3ad.chunk.css"
+  },
+  {
+    "revision": "c50955253534b6d2eb55",
+    "url": "/static/js/main.fb3c1937.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1318ccd47088b410594c",
+    "url": "/static/js/2.30905a5d.chunk.js"
+  },
+  {
+    "revision": "9796aa87081540f0a0bb0f98636a85f2",
+    "url": "/index.html"
+  }
+];
